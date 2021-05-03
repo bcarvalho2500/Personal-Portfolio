321a606fb30083f9a98d5a84a7f9c176a038db6f
Brandon Carvalho <32946299+bcarvalho2500@users.noreply.github.com>
2021-05-02 23:25:29 -0400

    Adds email

--- src/app/contactme/contactme.component.ts
+++ src/app/contactme/contactme.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contactme',
@@ -17,7 +18,7 @@ import { Component, OnInit } from '@angular/core';
 
     <section class="section">
       <div class="container">
-        <form (ngSubmit)="submitForm()">
+        <form #emailForm="ngForm" (ngSubmit)="submitForm(emailForm)">
           <!-- Name Input -->
           <div class="field">
             <label class="label">Name</label>
@@ -42,30 +43,75 @@ import { Component, OnInit } from '@angular/core';
             </div>
           </div>
 
-          <button type="submit" class="button is-info">Send!</button>
+          <button type="submit" class="button is-info" id="sendButton">Send!</button>
 
+          <div class="container field">
+            <p class="is-size-7 mt-3">Email is sent using <a href="https://postmail.invotes.com">Postmail</a></p>
+          </div>
         </form>
+
       </div>
+      
     </section>
+
   `,
   styles: [
   ]
 })
 export class ContactmeComponent implements OnInit {
-
+  
   name : String;
   email : String;
   message : String;
+  
+  form_id_js: String = "javascript_form";
+    
+  data_js = {
+    "access_token": "382k9i02fi8s729kbjlhyjt1"
+  };
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
-  submitForm(){
+  submitForm(emailForm : NgForm){
+
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (request.readyState == 4 && request.status == 200) {
+        alert("Message Sent!")
+      } else
+      if(request.readyState == 4) {
+        alert("Message Failed to Send")
+      }
+    };
+
+    // Message template
+    this.data_js['subject'] = "Email from Contact Me Form";
+    var text = this.message + "\n" + this.email + "\n\nFrom,\n" + this.name;
+    this.data_js['text'] = text;
+
+    var params = this.toParams(this.data_js);
+
+    request.open("POST", "https://postmail.invotes.com/send", true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    request.send(params);
     
-    const reply = `\n${this.name},\nThank you for using my contact me page!`
-    alert(reply) 
+    emailForm.reset();
+    
+    return false;
     
   }
+
+  toParams(data_js) {
+    var form_data = [];
+    for ( var key in data_js ) {
+        form_data.push(encodeURIComponent(key) + "=" + encodeURIComponent(data_js[key]));
+    }
+
+    return form_data.join("&");
+  } 
 }
